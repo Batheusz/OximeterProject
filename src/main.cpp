0a0b571af5a35df39e69e9319391a40c7c452cfd
Matheus Henrique Orsini <matheus1506200@outlook.com>
2023-09-08 18:29:05 -0300

    Done LCD layout

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,12 @@ int8_t validHeartRate;
 
 // Declaração de funções
 void drawText(char* text, uint8_t x, uint8_t y, uint16_t color, uint8_t size);
+void drawHeart();
+void drawBPM(uint16_t BPM, uint32_t color);
+void drawOxi();
+void drawSpO2(uint8_t oxi, uint32_t color);
+void drawTermometer();
+void drawTemp(float temp, uint32_t color);
 uint8_t oxiRead();
 void bttTrigg();
 
@@ -76,7 +82,7 @@ void setup() {
   ledcAttachPin(BLK,PWMCHANNEL); // Define o canal do PWM no controle do backlight
   MyLCD.initR(INITR_MINI160x80_PLUGIN); // Array de cores para o LCD
   MyLCD.setRotation(3); // Atualiza da esquerda para direita
-  ledcWrite(PWMCHANNEL, 1023); // Totalmente acesso
+  ledcWrite(PWMCHANNEL, 56); // Totalmente acesso
   MyLCD.fillScreen(ST7735_BLACK);
 
   // Inicializando MAX30102
@@ -100,7 +106,15 @@ void setup() {
   drawText("botao",3 ,30 , ST7735_WHITE, 2);
   vTaskDelay(500);
 
-  oxiRead();
+  MyLCD.fillScreen(ST7735_BLACK);
+  drawHeart();
+  drawBPM(888, ST7735_WHITE);
+  drawOxi();
+  drawSpO2(100, ST7735_WHITE);
+  drawTermometer();
+  drawTemp(99.6, ST7735_WHITE);
+  // oxiRead();
+
 }
 
 void loop() {
@@ -116,7 +130,7 @@ void bttTrigg()
 }
 /**************************************************************************/
 /*!
-    @brief Função para escrer o texto desejado  
+    @brief Função para escreverr o texto desejado  
     @param x Localização x do inicio do texto
     @param y Localização y do inicio do texto
     @param text String com o texto a ser escrito
@@ -131,6 +145,105 @@ void drawText(char* text, uint8_t x, uint8_t y, uint16_t color, uint8_t size)
   MyLCD.setTextWrap(1); // Quebra de linha automática
   MyLCD.setTextSize(size); // Tamanho da fonte
   MyLCD.print(text);
+}
+/**************************************************************************/
+/*!
+    @brief Função para desenhar o coração 
+*/
+/**************************************************************************/
+void drawHeart()
+{
+  // Desenha o coração
+  MyLCD.fillCircle(20,17,10,ST7735_RED);
+  MyLCD.fillCircle(40,17,10,ST7735_RED);
+  MyLCD.fillTriangle(10, 20, 30, 40, 50, 20, ST7735_RED);
+  // Desenha linha de pulso
+  MyLCD.drawLine(8, 25, 15, 25, ST7735_WHITE); // reta
+  MyLCD.drawLine(15, 25, 20, 15, ST7735_WHITE); // sobe
+  MyLCD.drawLine(20, 15, 25, 25, ST7735_WHITE); // desce
+  MyLCD.drawLine(25, 25, 28, 25, ST7735_WHITE); // reta
+  MyLCD.drawLine(28, 25, 32, 30, ST7735_WHITE); // desce
+  MyLCD.drawLine(32, 30, 35, 25, ST7735_WHITE); // sobe
+  MyLCD.drawLine(35, 25, 37, 25, ST7735_WHITE); // reta
+  MyLCD.drawLine(37, 25, 40, 18, ST7735_WHITE); // sobe
+  MyLCD.drawLine(40, 18, 44, 25, ST7735_WHITE); // desce
+  MyLCD.drawLine(44, 25, 52, 25, ST7735_WHITE); // reta
+  drawText("BPM",40 ,35 ,ST7735_WHITE, 1);
+}
+/**************************************************************************/
+/*!
+    @brief Função para escrever o BPM
+    @param BPM Valor a ser escrito
+    @param color Cor a ser usada
+*/
+/**************************************************************************/
+void drawBPM(uint16_t BPM, uint32_t color)
+{
+  MyLCD.setCursor(65,18);
+  MyLCD.setTextSize(2);
+  MyLCD.setTextColor(ST7735_WHITE);
+  MyLCD.print(BPM);
+}
+/**************************************************************************/
+/*!
+    @brief Função para desenhar oxigenação
+*/
+/**************************************************************************/
+void drawOxi()
+{
+  drawText("spO2:", 5, 55, ST7735_BLUE, 2);
+  drawText("%", 52, 70, ST7735_BLUE, 1);
+}
+/**************************************************************************/
+/*!
+    @brief Função para escrever a oxigenação
+    @param oxi Valor a ser escrito
+    @param color Cor a ser usada
+*/
+/**************************************************************************/
+void drawSpO2(uint8_t oxi, uint32_t color)
+{
+  MyLCD.setCursor(65,55);
+  MyLCD.setTextSize(2);
+  MyLCD.setTextColor(ST7735_WHITE);
+  MyLCD.print(oxi);
+}
+/**************************************************************************/
+/*!
+    @brief Função para desenhar o termometro
+*/
+/**************************************************************************/
+void drawTermometer()
+{
+  MyLCD.fillRoundRect(141,5,10,60,25,ST7735_WHITE);
+  MyLCD.fillCircle(145,65,10, ST7735_WHITE);
+  MyLCD.fillRoundRect(143,7,6,60,25,ST7735_BLACK);
+  MyLCD.fillRoundRect(143,30,6,40,25,ST7735_RED);
+  MyLCD.fillCircle(145,65,6, ST7735_RED);
+  // Divisões
+  MyLCD.fillRoundRect(141,50,7,2,25,ST7735_WHITE);
+  MyLCD.fillRoundRect(141,45,5,2,25,ST7735_WHITE);
+  MyLCD.fillRoundRect(141,40,7,2,25,ST7735_WHITE);
+  MyLCD.fillRoundRect(141,35,5,2,25,ST7735_WHITE);
+  MyLCD.fillRoundRect(141,30,7,2,25,ST7735_WHITE);
+  MyLCD.fillRoundRect(141,25,5,2,25,ST7735_WHITE);
+  MyLCD.fillRoundRect(141,20,7,2,25,ST7735_WHITE);
+  // Grau celsius
+  drawText("C", 132, 50, ST7735_WHITE, 1);
+}
+/**************************************************************************/
+/*!
+    @brief Função para escrever a temperatura
+    @param temp Valor a ser escrito
+    @param color Cor a ser usada
+*/
+/**************************************************************************/
+void drawTemp(float temp, uint32_t color)
+{
+  MyLCD.setCursor(90,35);
+  MyLCD.setTextSize(2);
+  MyLCD.setTextColor(color);
+  MyLCD.print(temp, 1);
 }
 /**************************************************************************/
 /*!
